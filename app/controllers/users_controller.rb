4c4d5900511ae69836685bcade38b3821c0f813e
Shawn Lee <Shawnl93@hotmail.com>
2022-12-12 22:41:28 -0700

    dont understand why its not redirecting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    # @user = User.new(user_params)
-    # if @user.save
-    #   redirect_to user_path(@user.id)
-    # else
-    #   flash[:alert] = @user.errors.full_messages
-    #   redirect_to new_register_path
-    # end
-    user = User.create(user_params)
-    flash[:success] = "Welcome, #{user.users}!"
-    redirect_to user_path(user.id)
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      flash[:alert] = @user.errors.full_messages
+      redirect_to new_register_path
+    end
+    # user = User.create(user_params)
+    # flash[:success] = "Welcome, #{user.users}!"
+    # redirect_to user_path(user)
   end
 
 private
